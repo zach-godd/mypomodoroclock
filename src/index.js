967d56b1004f67f742f4a95c83f5c5a6fef46424
Zachary Goddard <zath0492@gmail.com>
2020-08-23 23:53:53 -0500

    test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Timer from "./Timer.js";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Timer />
   </React.StrictMode>,
   document.getElementById('root')
 );
